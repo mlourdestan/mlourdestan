09ee11f1bd0ed16bbaafdcec58cdfb7124effe75
Tan <me@mlourdestan.com>
2023-01-19 23:16:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,106 @@
-$(window).load(function () {
-  var body = $("body"),
-    universe = $("#universe"),
-    solarsys = $("#solar-system");
+"use strict";
 
-  var init = function () {
-    body
-      .removeClass("view-2D opening")
-      .addClass("view-3D")
-      .delay(2000)
-      .queue(function () {
-        $(this).removeClass("hide-UI").addClass("set-speed");
-        $(this).dequeue();
-      });
-  };
+var canvas = document.getElementById("canvas"),
+  ctx = canvas.getContext("2d"),
+  w = (canvas.width = window.innerWidth),
+  h = (canvas.height = window.innerHeight),
+  hue = 217,
+  stars = [],
+  count = 0,
+  maxStars = 1400;
 
-  var setView = function (view) {
-    universe.removeClass().addClass(view);
-  };
+// Thanks @jackrugile for the performance tip! https://codepen.io/jackrugile/pen/BjBGoM
+// Cache gradient
+var canvas2 = document.createElement("canvas"),
+  ctx2 = canvas2.getContext("2d");
+canvas2.width = 100;
+canvas2.height = 100;
+var half = canvas2.width / 2,
+  gradient2 = ctx2.createRadialGradient(half, half, 0, half, half, half);
+gradient2.addColorStop(0.025, "#fff");
+gradient2.addColorStop(0.1, "hsl(" + hue + ", 61%, 33%)");
+gradient2.addColorStop(0.25, "hsl(" + hue + ", 64%, 6%)");
+gradient2.addColorStop(1, "transparent");
 
-  $("#toggle-data").click(function (e) {
-    body.toggleClass("data-open data-close");
-    e.preventDefault();
-  });
+ctx2.fillStyle = gradient2;
+ctx2.beginPath();
+ctx2.arc(half, half, half, 0, Math.PI * 2);
+ctx2.fill();
 
-  $("#toggle-controls").click(function (e) {
-    body.toggleClass("controls-open controls-close");
-    e.preventDefault();
-  });
+// End cache
 
-  $("#data a").click(function (e) {
-    var ref = $(this).attr("class");
-    solarsys.removeClass().addClass(ref);
-    $(this).parent().find("a").removeClass("active");
-    $(this).addClass("active");
-    e.preventDefault();
-  });
+function random(min, max) {
+  if (arguments.length < 2) {
+    max = min;
+    min = 0;
+  }
 
-  $(".set-view").click(function () {
-    body.toggleClass("view-3D view-2D");
-  });
-  $(".set-zoom").click(function () {
-    body.toggleClass("zoom-large zoom-close");
-  });
-  $(".set-speed").click(function () {
-    setView("scale-stretched set-speed");
-  });
-  $(".set-size").click(function () {
-    setView("scale-s set-size");
-  });
-  $(".set-distance").click(function () {
-    setView("scale-d set-distance");
-  });
+  if (min > max) {
+    var hold = max;
+    max = min;
+    min = hold;
+  }
 
-  init();
-});
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function maxOrbit(x, y) {
+  var max = Math.max(x, y),
+    diameter = Math.round(Math.sqrt(max * max + max * max));
+  return diameter / 2;
+}
+
+var Star = function () {
+  this.orbitRadius = random(maxOrbit(w, h));
+  this.radius = random(60, this.orbitRadius) / 12;
+  this.orbitX = w / 2;
+  this.orbitY = h / 2;
+  this.timePassed = random(0, maxStars);
+  this.speed = random(this.orbitRadius) / 50000;
+  this.alpha = random(2, 10) / 10;
+
+  count++;
+  stars[count] = this;
+};
+
+Star.prototype.draw = function () {
+  var x = Math.sin(this.timePassed) * this.orbitRadius + this.orbitX,
+    y = Math.cos(this.timePassed) * this.orbitRadius + this.orbitY,
+    twinkle = random(10);
+
+  if (twinkle === 1 && this.alpha > 0) {
+    this.alpha -= 0.05;
+  } else if (twinkle === 2 && this.alpha < 1) {
+    this.alpha += 0.05;
+  }
+
+  ctx.globalAlpha = this.alpha;
+  ctx.drawImage(
+    canvas2,
+    x - this.radius / 2,
+    y - this.radius / 2,
+    this.radius,
+    this.radius
+  );
+  this.timePassed += this.speed;
+};
+
+for (var i = 0; i < maxStars; i++) {
+  new Star();
+}
+
+function animation() {
+  ctx.globalCompositeOperation = "source-over";
+  ctx.globalAlpha = 0.8;
+  ctx.fillStyle = "hsla(" + hue + ", 64%, 6%, 1)";
+  ctx.fillRect(0, 0, w, h);
+
+  ctx.globalCompositeOperation = "lighter";
+  for (var i = 1, l = stars.length; i < l; i++) {
+    stars[i].draw();
+  }
+
+  window.requestAnimationFrame(animation);
+}
+
+animation();
